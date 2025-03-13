905e2f7a70000c04687d1caa8d84abc63e828181
wanshidayi <wpa4755911@126.com>
2025-03-14 02:11:21 +0800

    Initial commit

---
+++ data/data.js
@@ -0,0 +1,151 @@
+var baseURL = ''
+var data = {
+    hot: [
+        {
+            name: '嘟嘟妹',
+            view: '31589',
+            cover: baseURL + 'data/hot/play.gif'
+        },
+        {
+            name: '飞儿',
+            view: '7572',
+            cover: baseURL + 'data/hot/1.jpg'
+        },
+        {
+            name: '销魂姐姐',
+            view: '6896',
+            cover: baseURL + 'data/hot/2.jpg'
+        },
+        {
+            name: '不要啦',
+            view: '6628',
+            cover: baseURL + 'data/hot/3.jpg'
+        },
+        {
+            name: '小嫩粉出水',
+            view: '6034',
+            cover: baseURL + 'data/hot/4.jpg'
+        },
+        {
+            name: '晓彤',
+            view: '27582',
+            cover: baseURL + 'data/hot/5.gif'
+        },      
+        {
+            name: '欣欣酱开课',
+            view: '5641',
+            cover: baseURL + 'data/hot/5.jpg'
+        },
+        {
+            name: '下面空空',
+            view: '6896',
+            cover: baseURL + 'data/hot/6.jpg'
+        },
+        {
+            name: '小水水',
+            view: '6628',
+            cover: baseURL + 'data/hot/7.jpg'
+        },
+        {
+            name: '柠檬怪',
+            view: '6034',
+            cover: baseURL + 'data/hot/8.jpg'
+        },
+        {
+            name: '学生妹妹',
+            view: '5641',
+            cover: baseURL + 'data/hot/9.jpg'
+        },
+        {
+            name: '宝宝爱吃鱼',
+            view: '6896',
+            cover: baseURL + 'data/hot/10.jpg'
+        },
+        {
+            name: '血小板',
+            view: '6628',
+            cover: baseURL + 'data/hot/11.jpg'
+        },
+        {
+            name: '小水女',
+            view: '6034',
+            cover: baseURL + 'data/hot/12.jpg'
+        },
+        {
+            name: '小羊羔',
+            view: '5641',
+            cover: baseURL + 'data/hot/13.jpg'
+        },
+        {
+            name: '下面给你吃',
+            view: '6628',
+            cover: baseURL + 'data/hot/15.jpg'
+        },
+        {
+            name: '萝莉戏水',
+            view: '6034',
+            cover: baseURL + 'data/hot/16.jpg'
+        },    
+        {
+            name: '户外爱好者',
+            view: '5035',
+            cover: baseURL + 'data/hot/18.jpg'
+        }
+    ],
+    video: [
+        {
+            name: '可骚可萌',
+            cover: baseURL + 'data/video/1.jpg'
+        },
+        {
+            name: '性感小尤物',
+            cover: baseURL + 'data/video/2.jpg'
+        },
+        {
+            name: '粉嫩',
+            cover: baseURL + 'data/video/3.jpg'
+        },
+        {
+            name: '乃大妹妹',
+            cover: baseURL + 'data/video/4.jpg'
+        },
+        {
+            name: '迷情小森林',
+            view: '5641',
+            cover: baseURL + 'data/video/5.gif'
+        },
+        {
+            name: '丝袜秀儿',
+            view: '5641',
+            cover: baseURL + 'data/video/6.jpg'
+        },
+        {
+            name: '摸摸大',
+            view: '5641',
+            cover: baseURL + 'data/video/7.jpg'
+        },
+        {
+            name: '萌萌大宝贝',
+            view: '5641',
+            cover: baseURL + 'data/video/8.jpg'
+        },
+        {
+            name: '性感少妇',
+            cover: baseURL + 'data/video/9.jpg'
+        },
+        {
+            name: '诱人的樱桃',
+            view: '5641',
+            cover: baseURL + 'data/video/10.jpg'
+        },
+        {
+            name: '小可怜',
+            view: '5641',
+            cover: baseURL + 'data/video/11.jpg'
+        },      
+        {
+            name: '软球球',
+            cover: baseURL + 'data/video/12.jpg'
+        }
+    ]
+}
